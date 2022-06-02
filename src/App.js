61dfc28f73d1c1e2644678a632ef04211bcc2bb7
Lina Z <lina.lyden@gmail.com>
2022-06-02 12:59:52 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    //to make it more accessible hide this for the screen reader and then have another heading for
+    <div className="App">
+      <h1 className="rainbow-letters">
+        RAINBOWWWWWW
+        {/* <span>R</span>
+        <span>A</span>
+        <span>I</span>
+        <span>N</span>
+        <span>B</span>
+        <span>O</span>
+        <span>W</span>
+        <span>W</span>
+        <span>W</span>
+        <span>W</span>
+        <span>W</span>
+        <span>W</span>
+        <span>W</span>
+        <span>W</span> */}
+      </h1>
+    </div>
+  );
+}
